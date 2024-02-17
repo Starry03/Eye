e8f2f1ca44a461c5181cc33430cfdd913a36877a
Starry03 <andrea.starrantino@gmail.com>
2024-02-17 20:14:29 +0100

    bo poco

--- src/Eye/package-info.java
+++ src/Eye/package-info.java
@@ -1,1 +1,11 @@
+/**
+ * Eye
+ * Manages endpoints and requests.
+ * Provides classes to handle different kind of responses.
+ *
+ * @since 1.0
+ * @author Andrea Starrantino
+ * @version 1.0
+ */
+
 package Eye;
